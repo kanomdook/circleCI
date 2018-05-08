594d605816ca35fa38f7d789322ccb6535a24c9a
kanomdook <kanung.2535@gmail.com>
2018-05-08 12:23:53 +0700

    delete file

--- index.js
+++
@@ -1,4 +0,0 @@
-exports.plus = function (a, b) { return a + b; };
-exports.minus = function (a, b) { return a - b; };
-exports.multiply = function (a, b) { return a * b; };
-exports.devide = function (a, b) { return a / b; };
